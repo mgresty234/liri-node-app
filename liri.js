546c44688067a3482ceab12287dd70d51e64281a
mgresty234 <mgresty223@gmail.com>
2018-05-14 15:51:18 -0400

    update

--- liri.js
+++ liri.js
@@ -4,5 +4,5 @@ import 'keys.js';
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-// Make so liri can take in one of the following commands:
-// `my-tweets`, `spotify-this-song`, `movie-this`, `do-what-it-says`
+// Make so liri can take in the following commands:
+// `my-tweets`, `spotify-this-song`
